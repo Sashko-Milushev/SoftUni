5cc6d8a7107483089953589c0f258cc0bc57ec3c
Sashko Milushev <93657471+Sashko-Milushev@users.noreply.github.com>
2021-11-15 23:17:55 +0200

    Add files via upload

---
+++ convert_text.py
@@ -0,0 +1,19 @@
+import pywhatkit as pywhatkit
+
+import pywhatkit
+pywhatkit.text_to_handwriting("It's been one of those days\
+Where things don't go my way\
+Ooh, and it seems like always\
+The thing to blame is my ways\
+You're the one I'm thinkin' of\
+The one I'm dreamin' of\
+You are my lady\
+I am your man\
+Some call it crazy\
+They don't understand\
+When I'm gone\
+Baby, I'm always gone too long\
+That don't get your mind to wonderin'\
+If I still care, 'cause, baby, I still care\
+And you're the one I'm thinkin' of\
+The one I'm dreamin' of is you, oh...", rgb=(36, 112, 77))
---
+++ demo.py
@@ -0,0 +1,2 @@
+phrase = "Giraffe Academy"
+print(phrase.upper().isupper())
