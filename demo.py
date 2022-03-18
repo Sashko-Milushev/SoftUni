56143b8d07d69cba945b7714c00947cb17a2f9d3
Sashko Milushev <93657471+Sashko-Milushev@users.noreply.github.com>
2022-03-18 11:04:13 +0200

    Add files via upload

--- demo.py
+++ demo.py
@@ -1,2 +1,13 @@
-phrase = "Giraffe Academy"
-print(phrase.upper().isupper())
+from wild_cat_zoo.pokemon import Pokemon
+from wild_cat_zoo.trainer import Trainer
+
+pokemon = Pokemon("Pikachu", 90)
+print(pokemon.pokemon_details())
+trainer = Trainer("Ash")
+print(trainer.add_pokemon(pokemon))
+second_pokemon = Pokemon("Charizard", 110)
+print(trainer.add_pokemon(second_pokemon))
+print(trainer.add_pokemon(second_pokemon))
+print(trainer.release_pokemon("Pikachu"))
+print(trainer.release_pokemon("Pikachu"))
+print(trainer.trainer_data())
